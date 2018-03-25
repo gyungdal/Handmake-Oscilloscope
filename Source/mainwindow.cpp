2fc26951be50b6cbdda02a0df004006da552d62a
Gyeongsik Kim <admin@gyungdal.cc>
2018-03-25 22:29:40 +0900

    UI 작성중...

--- Source/mainwindow.cpp
+++ Source/mainwindow.cpp
@@ -4,6 +4,8 @@ QT_CHARTS_USE_NAMESPACE
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent) {
+    const QString temp("±1.8V,±5V,±18V,±180V");
+    scaleList = temp.split(",");
     setupChartView();
 }
 
@@ -12,12 +14,12 @@ void MainWindow::setupChartView(){
     QLineSeries *series = new QLineSeries();
     *series << QPointF(1, 0.5) << QPointF(3, .6) << QPointF(7, -0.6) << QPointF(9, -0.7)
         << QPointF(12, .6) << QPointF(16, .7) << QPointF(18, .5);
-
     series->setName("Channel 0");
 
     QLineSeries *series3 = new QLineSeries();
     *series3 << QPointF(1, -0.5) << QPointF(2, -2) << QPointF(6, 1.3);
     series3->setName("Channel 3");
+
     QChart* chart = new QChart();
     chart->addSeries(series);
     chart->addSeries(series3);
@@ -25,13 +27,54 @@ void MainWindow::setupChartView(){
     chart->createDefaultAxes();
     chart->axisX()->setRange(0, 20);
     chart->axisY()->setRange(-2, 2);
-    QChartView *chartView = new QChartView(chart);
+    chart->setTheme(QChart::ChartThemeDark);
+    chartView = new QChartView(chart);
     chartView->setRenderHint(QPainter::Antialiasing);
 
-    this->setCentralWidget(chartView);
+    mainLayout = new QGridLayout();
+    toolLayout = new QGridLayout();
+
+    scaleDial = new QDial();
+    scaleDial->setMinimum(0);
+    scaleDial->setMaximum(3);
+
+    speedDial = new QDial();
+    speedDial->setMinimum(0);
+    speedDial->setMaximum(10);
+
+    scaleView = new QLabel();
+    scaleView->setText(scaleList[0]);
+    speedView = new QLabel();
+    speedView->setText("10ms");
+    sampleFreqView = new QLabel();
+    sampleFreqView->setText("1000/s");
+
+    toolLayout->addWidget(scaleView, 0, 0);
+    toolLayout->addWidget(scaleDial, 0, 1);
+    toolLayout->addWidget(speedView, 0, 2);
+    toolLayout->addWidget(speedDial, 0, 3);
+    toolLayout->addWidget(sampleFreqView, 0, 4);
+
+    mainLayout->addWidget(chartView, 0, 0);
+    //mainLayout->addLayout(toolLayout, 0, 1);
+
+    this->setLayout(mainLayout);
     this->resize(1280, 720);
 }
 
-MainWindow::~MainWindow() {
+void MainWindow::scaleDialEventHadler(int value){
+    scaleView->setText(scaleList[value]);
+}
 
+void MainWindow::speedDialEventHadler(int value){
+    char* text = new char[50];
+    itoa(value, text, 10);
+    speedView->setText(text);
+    delete text;
+}
+
+MainWindow::~MainWindow() {
+    delete chartView;
+    delete toolLayout;
+    delete mainLayout;
 }
--- Source/mainwindow.h
+++ Source/mainwindow.h
@@ -3,6 +3,7 @@
 
 #include <QMainWindow>
 #include <QtCharts>
+#include <vector>
 
 QT_CHARTS_USE_NAMESPACE
 
@@ -10,18 +11,38 @@ class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
+    //ADC Item struct
     typedef struct {
         uint16_t raw : 12;
         uint16_t reverse : 1;
         uint16_t channel : 3;
         double voltage;
     } adc_item_t;
+
     MainWindow(QWidget *parent = 0);
+
     ~MainWindow();
 private:
-    QChartView chart;
-    void setupChartView();
+    QStringList scaleList;
 
+    //Dial
+    QDial* scaleDial;
+    QDial* speedDial;
+
+    //Infomation View
+    QLabel* scaleView;
+    QLabel* speedView;
+    QLabel* sampleFreqView;
+
+    //Chart!
+    QChartView* chartView;
+
+    QGridLayout* toolLayout;
+    QGridLayout * mainLayout;
+
+    void setupChartView();
+    void scaleDialEventHadler(int);
+    void speedDialEventHadler(int);
 };
 
 #endif // MAINWINDOW_H
